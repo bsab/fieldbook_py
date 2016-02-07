283c0cfbbdcc2287d4fc1a86188c3f8019754260
Matt Stibbs <git@stibbsy.co.uk>
2016-02-07 15:19:41 +0000

    Add setup.py for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fieldbook_py',
+      version='0.1',
+      description='Helper package for using the Fieldbook.com API',
+      url='http://github.com/mattstibbs/fieldbook_py',
+      author='Matt Stibbs',
+      author_email='git@stibbsy.co.uk',
+      license='MIT',
+      packages=['fieldbook_py'],
+      zip_safe=False)
