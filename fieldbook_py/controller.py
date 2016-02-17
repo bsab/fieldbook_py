2c1b334f7bac36db5da0b9504d1fb8f0741fe22e
Matt Stibbs <git@stibbsy.co.uk>
2016-02-17 08:33:01 +0000

    Add read, update, and add methods

--- fieldbook_py/controller.py
+++ fieldbook_py/controller.py
@@ -21,4 +21,43 @@ class FieldbookClient(object):
         pass
 
     def get_rows(self, sheet):
-        pass
+        url = str.format('{0}{1}{2}', self.__url, '/', sheet)
+        print(url)
+
+        try:
+            request = requests.get(url,
+                                   auth=(self.__key, self.__secret))
+            return request.json()
+
+        except requests.ConnectionError as e:
+            log.error('Cannot connect to Fieldbook')
+            log.error(e)
+
+        except Exception as e:
+            log.error(e)
+
+    def add_row(self, sheet, new_record):
+
+        url = str.format('{0}{1}{2}', self.__url, '/', sheet)
+        print(url)
+
+        request = requests.post(url, auth=(self.__key, self.__secret), json = new_record)
+        print(request.status_code)
+
+        result = json.loads(request.text)
+        print(result)
+
+        return result
+
+    def update_row(self, sheet, patch_id, patch_record):
+
+        url = str.format('{0}{1}{2}{3}', self.__url, '/', sheet, '/', patch_id)
+        print(url)
+
+        request = requests.patch(url, auth=(self.__key, self.__secret), json = patch_record)
+        print(request.status_code)
+
+        result = json.loads(request.text)
+        print(result)
+
+        return result
