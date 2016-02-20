93873b1f114edfb413e87c5db720a299588d74f2
Matt Stibbs <git@stibbsy.co.uk>
2016-02-20 14:37:47 +0000

    Update logging configuration

--- fieldbook_py/__init__.py
+++ fieldbook_py/__init__.py
@@ -1,11 +1,10 @@
-from .controller import *
+from fieldbook import *
 import logging
 
 # Set up logging
-log = logging.getLogger('fieldbook-py')
-log.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-log.addHandler(ch)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logger.info("Is it alive?")
 
 alive()
