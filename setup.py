784bb64194e409974c782c889451e5c03548b668
Matt Stibbs <git@stibbsy.co.uk>
2017-03-04 22:51:47 +0000

    Remove python 3 support

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='fieldbook_py',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython'
       ],
