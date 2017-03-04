e9374d11684ab30e24572642be9fc79a4136abcb
Matt Stibbs <git@stibbsy.co.uk>
2017-03-04 22:53:13 +0000

    Uplift to version 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fieldbook_py',
-      version='0.2.8',
+      version='0.3',
       description='Helper package for using the Fieldbook.com API',
       classifiers=[
         'Development Status :: 4 - Beta',
